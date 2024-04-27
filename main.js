e399f8de2904ea21bb658e7d0b256c3008b19fc0
kauan araujo <kauanraujo340@gmail.com>
2024-04-26 23:23:49 -0300

    tudo funcionando legal

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form-agenda');
+const imgSalvo = '<img src="./img_projeto/agenda_png.png" alt="iamgem salvo"/>';
 
 let linhas = '';
 
@@ -11,6 +12,7 @@ form.addEventListener('submit', function(e) {
     let linha = '<tr>';
     linha += `<td>${inputNomeContato.value} </td>`;
     linha += `<td>${inpoutNumeroContato.value}</td>`;
+    linha += `<td> ${inpoutNumeroContato.value = imgSalvo} </td>`
     linha += '<tr>';
 
     linhas += linha;
